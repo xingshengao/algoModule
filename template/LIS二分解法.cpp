3833b599d266b3998caa6ac5529eea03c2bb8b3e
Khosaka Reina <2211916133@qq.com>
2023-11-14 16:34:49 +0800

    111

---
+++ template/LIS二分解法.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 严格递增的LIS子序列, 二分+贪心
+template <typename T>
+int lis(vector<T> A) {
+    vector<T> dp;
+    for (auto x : A) {
+        auto it = lower_bound(dp.begin(), dp.end(), x);
+        if (x == dp.end())
+            dp.push_back(x);
+        else
+            *it = x;
+    }
+    return dp.size();
+}
+// 非递减的LIS子序列, 二分+贪心
+template <typename T>
+int lis(vector<T> A) {
+    vector<T> dp;
+    for (auto x : A) {
+        auto it = upper_bound(dp.begin(), dp.end(), x);
+        if (x == dp.end())
+            dp.push_back(x);
+        else
+            *it = x;
+    }
+    return dp.size();
+}
+int main() {}
