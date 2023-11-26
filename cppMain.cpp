b084a1280c73fed2d603602970ed4bd7599a26d1
Khosaka Reina <2211916133@qq.com>
2023-11-26 19:03:03 +0800

    1126

--- cppMain.cpp
+++ cppMain.cpp
@@ -55,20 +55,51 @@ void mydebug(const char* format, Head H, Tail... T) {
 #define debug(...) mydebug(#__VA_ARGS__, __VA_ARGS__)
 
 static constexpr long long mod = 998244353;
-
-void solve() {
-    int a, b, x;
-    cin >> a >> b >> x;
-    int g = gcd(a, b);
-    if (g == 1) {
-        cout << x << endl;
-        return;
+int mypow(int x, int n) {
+    int res = 1;
+    for (; n; n /= 2) {
+        if (n & 1) res = res * x;
+        x = x * x;
     }
-    int m = x / g * g, n = m + g;
-    if (x - m <= n - x)
-        cout << m;
-    else
-        cout << n;
+    return res;
+}
+void solve() {
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    vector<int> s(m + 1);
+    vector<int> b(n + 5);            // 差分数组
+    vector<vector<int>> pos(m + 1);  // 存储宝石放置的位置
+
+    for (int i = 1; i <= m; i++) cin >> s[i];  // 输入宝石的能量
+
+    for (int i = 0; i < q; i++) {
+        int p, k;
+        cin >> p >> k;
+        pos[p].push_back(k);
+    }
+
+    for (int i = 1; i <= m; i++) {
+        if (pos[i].size() == 0) {
+            continue;
+        }
+        sort(pos[i].begin(), pos[i].end());
+        int l = pos[i][0], r = l + s[i] - 1;
+        for (int j = 1; j < pos[i].size(); j++) {
+            if (pos[i][j] <= r) {
+                r = pos[i][j] + s[i] - 1;
+            } else {
+                b[l] += 1, b[min(r + 1, n + 1)] -= 1;
+                l = pos[i][j], r = l + s[i] - 1;
+            }
+        }
+        b[l] += 1, b[min(n + 1, r + 1)] -= 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        b[i] += b[i - 1];
+        cout << b[i] << " \n"[i == n];
+    }
 }
 signed main() {
     std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
