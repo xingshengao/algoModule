87bb57d7cd4542fe3181cf7b6f10c394684b5d4f
Khosaka Reina <2211916133@qq.com>
2023-11-26 19:05:32 +0800

    11.26

--- cppMain.cpp
+++ cppMain.cpp
@@ -63,44 +63,7 @@ int mypow(int x, int n) {
     }
     return res;
 }
-void solve() {
-    int n, m, q;
-    cin >> n >> m >> q;
-
-    vector<int> s(m + 1);
-    vector<int> b(n + 5);            // 差分数组
-    vector<vector<int>> pos(m + 1);  // 存储宝石放置的位置
-
-    for (int i = 1; i <= m; i++) cin >> s[i];  // 输入宝石的能量
-
-    for (int i = 0; i < q; i++) {
-        int p, k;
-        cin >> p >> k;
-        pos[p].push_back(k);
-    }
-
-    for (int i = 1; i <= m; i++) {
-        if (pos[i].size() == 0) {
-            continue;
-        }
-        sort(pos[i].begin(), pos[i].end());
-        int l = pos[i][0], r = l + s[i] - 1;
-        for (int j = 1; j < pos[i].size(); j++) {
-            if (pos[i][j] <= r) {
-                r = pos[i][j] + s[i] - 1;
-            } else {
-                b[l] += 1, b[min(r + 1, n + 1)] -= 1;
-                l = pos[i][j], r = l + s[i] - 1;
-            }
-        }
-        b[l] += 1, b[min(n + 1, r + 1)] -= 1;
-    }
-
-    for (int i = 1; i <= n; i++) {
-        b[i] += b[i - 1];
-        cout << b[i] << " \n"[i == n];
-    }
-}
+void solve() {}
 signed main() {
     std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
     int T = 1;
