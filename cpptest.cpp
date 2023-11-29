bd6c557a0a34a7f0f49785f1ee9b3806a1e96d9f
Khosaka Reina <2211916133@qq.com>
2023-11-29 19:03:16 +0800

    11.28

--- cpptest.cpp
+++ cpptest.cpp
@@ -71,8 +71,41 @@ typedef pair<int, int> PII;
 vector<PII> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 static constexpr long long mod = 1e9 + 7;
 using LL = long long;
-
+class Solution {
+   public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<array<int, 3>> vec(n);
+        for (int i = 0; i < n; ++i) {
+            vec[i] = {startTime[i], endTime[i], profit[i]};
+        }
+        // 按照结束时间升序排序
+        sort(vec.begin(), vec.end(), [&](auto& a, auto& b) { return a[1] < b[1]; });
+        vector<int> dp(n + 1, -1);  // dp[i]表示前i个工作的最大报酬
+        function<int(int)> dfs = [&](int i) -> int {
+            int& res = dp[i];
+            if (res != -1) return res;
+            if (i == 0) return res = 0;
+            int up = vec[i - 1][0];  // 第i个的开始时间
+            int l = 0, r = i - 1;
+            while (l < r) {
+                int mid = l + (r - l + 1 >> 1);
+                if (vec[mid][1] <= up)
+                    l = mid;
+                else
+                    r = mid - 1;
+            }
+            res = max(dfs(i - 1), vec[i - 1][2]);
+            if (vec[r][1] <= up) res = max(res, dfs(r + 1) + vec[i - 1][2]);
+            return res;
+        };
+        for (int i = 0; i <= n; ++i) dfs(i);
+        debug(dp);
+        return dfs(n);
+    }
+};
 int main() {
     // Solotion so;
+
     return 0;
 }
