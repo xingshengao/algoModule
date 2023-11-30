88b9434419dc4ccc423d523c683ef57ff5f4f1aa
Khosaka Reina <2211916133@qq.com>
2023-11-30 19:19:28 +0800

    11.30

---
+++ template/并查集/带cnt的并查集.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DSU {
+    vector<int> f, siz;
+    int cnt;
+    DSU() {}
+    DSU(int n) { init(n); }
+    void init(int n) {
+        cnt = n;
+        f.resize(n);
+        iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
+    }
+    int find(int x) {
+        while (x != f[x]) {
+            x = f[x] = f[f[x]];
+        }
+        return x;
+    }
+    bool same(int x, int y) { return find(x) == find(y); }
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return false;
+        }
+        cnt--;
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    int size(int x) { return siz[find(x)]; }
+};
