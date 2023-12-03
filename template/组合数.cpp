672d687d62cdd43a298790e123d1f0fd6d293977
Khosaka Reina <2211916133@qq.com>
2023-12-03 17:48:23 +0800

    12.03

---
+++ template/组合数.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+typedef long long LL;
+namespace Comb {
+constexpr LL N = 1e5 + 50;
+constexpr LL mod = 1e9 + 7;
+LL frac[N];  // 阶乘
+
+void init() {
+    frac[0] = frac[1] = 1;
+    for (int i = 2; i < N; ++i) frac[i] = frac[i - 1] * i % mod;
+}
+
+LL fpow(LL x, LL exp) {  // 快速幂
+    LL res = 1;
+    for (; exp; exp /= 2) {
+        if (exp & 1) res = res * x % mod;
+        x = x * x % mod;
+    }
+    return res;
+}
+
+/* C(n, k) means select k from n. */
+int C(int n, int k) {
+    LL upper = frac[n];
+    LL lower = (fpow(frac[k], mod - 2) * fpow(frac[n - k], mod - 2)) % mod;  // 逆元
+
+    return (upper * lower) % mod;
+}
+};  // namespace Comb
+using namespace Comb;
+int main() {
+    // 使用举例
+    init();
+    std::cout << C(4, 2) << std::endl;
+}
