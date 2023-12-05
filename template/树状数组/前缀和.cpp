066924aac199365f161999824f5b2c0f6a56a7f9
Khosaka Reina <2211916133@qq.com>
2023-12-05 15:33:53 +0800

    12.05

---
+++ template/树状数组/前缀和.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+class Solution {
+public:
+    LL tr[100010]{0}; // 维护前缀和
+    LL lowbit(LL x) {return x & (-x);}
+    void add(LL x, LL v) {
+        for (; x < 100010; x += lowbit(x)) tr[x] += v;
+    }
+    LL query(LL x) {
+        LL res = 0;
+        for (; x; x -= lowbit(x)) res += tr[x];
+        return res;
+    }
+    LL sumRange(LL l, LL r) { // [L, R]的和, 下标都从1开始
+        return query(r) - query(l - 1);
+    }
+
+};
