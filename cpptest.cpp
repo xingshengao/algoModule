be8d62ea0b5dbe0e225bc119bc67cd52950e953f
Khosaka Reina <2211916133@qq.com>
2023-12-11 16:00:06 +0800

    1211

--- cpptest.cpp
+++ cpptest.cpp
@@ -66,55 +66,7 @@ int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
 typedef pair<int, int> PII;
 static constexpr long long mod = 1e9 + 7;
 using LL = long long;
-// Tarjan算法求无向图中的强连通分量
-class Solution {
-   public:
-    int time;                 // 时间戳
-    vector<vector<int>> adj;  // 邻接表
-    vector<int> dfn;          // 节点的时间戳
-    vector<int> low;          // 节点的追溯值
-    vector<bool> vis;         // 是否遍历过节点
-    int cnt;                  // 强连通分量的数量
-    vector<vector<int>> ans;  //  存强连通分量
-    void tarjan(int u, int fa) {
-        time++;
-        dfn[u] = time;
-        low[u] = time;
-        vis[u] = 1;
 
-        for (int v : adj[u]) {
-            if (v == fa) continue;
-            if (!vis[v]) {
-                tarjan(v, u);
-                low[u] = min(low[u], low[v]);
-                // 说明找到了一条强连通分量
-                if (low[v] > dfn[u]) {
-                    ans.push_back({u, v});
-                    cnt += 1;
-                }
-            } else {
-                low[u] = min(low[u], dfn[v]);
-            }
-        }
-    }
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        // 初始化工具人数组
-        time = 0;
-        adj = vector<vector<int>>(n);
-        dfn = vector<int>(n, 0);
-        low = vector<int>(n, 0);
-        vis = vector<bool>(n, false);
-        cnt = 0;
-        // 建图
-        for (auto& e : connections) {
-            int x = e[0], y = e[1];
-            adj[x].push_back(y);
-            adj[y].push_back(x);
-        }
-        tarjan(0, -1);
-        return ans;
-    }
-};
 int main() {
     // Solotion so;
     return 0;
