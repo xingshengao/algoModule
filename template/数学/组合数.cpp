c9e3385648c55ce1591fd9ec2bc90115b54b19c7
Khosaka Reina <2211916133@qq.com>
2023-12-12 16:49:57 +0800

    1212

--- template/数学/组合数.cpp
+++ template/数学/组合数.cpp
@@ -2,19 +2,19 @@
 typedef long long LL;
 namespace Comb {
 constexpr LL N = 1e5 + 50;
-constexpr LL mod = 1e9 + 7;
+constexpr LL MOD = 1e9 + 7;
 LL frac[N];  // 阶乘
 
 void init() {
     frac[0] = frac[1] = 1;
-    for (LL i = 2; i < N; ++i) frac[i] = frac[i - 1] * i % mod;
+    for (LL i = 2; i < N; ++i) frac[i] = frac[i - 1] * i % MOD;
 }
 
 LL fpow(LL x, LL exp) {  // 快速幂
     LL res = 1;
     for (; exp; exp /= 2) {
-        if (exp & 1) res = res * x % mod;
-        x = x * x % mod;
+        if (exp & 1) res = res * x % MOD;
+        x = x * x % MOD;
     }
     return res;
 }
@@ -22,9 +22,9 @@ LL fpow(LL x, LL exp) {  // 快速幂
 /* C(n, k) means select k from n. */
 LL C(LL n, LL k) {
     LL upper = frac[n];
-    LL lower = (fpow(frac[k], mod - 2) * fpow(frac[n - k], mod - 2)) % mod;  // 逆元
+    LL lower = (fpow(frac[k], MOD - 2) * fpow(frac[n - k], MOD - 2)) % MOD;  // 逆元
 
-    return (upper * lower) % mod;
+    return (upper * lower) % MOD;
 }
 };  // namespace Comb
 using namespace Comb;
