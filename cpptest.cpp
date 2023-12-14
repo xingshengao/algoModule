0cdf7b1e303e484c516010c7b935505162416f49
Khosaka Reina <2211916133@qq.com>
2023-12-14 15:15:51 +0800

    1555

--- cpptest.cpp
+++ cpptest.cpp
@@ -88,8 +88,7 @@ class Solution {
    public:
     int deleteString(string s) {
         int n = s.size();
-        int lcp[n][n];
-        memset(lcp, 0, sizeof lcp);
+        vector lcp(n, vector<int>(n, 0));
         for (int j = n - 1; j >= 0; --j) {
             for (int i = n - 1; i >= 0; --i) {
                 if (s[i] == s[j]) {
@@ -98,6 +97,18 @@ class Solution {
                 }
             }
         }
+        vector<int> dp(n + 1, -1);
+        function<int(int)> dfs = [&](int i) -> int {
+            if (i == n - 1) return 1;
+            int& res = dp[i];
+            if (res != -1) return res;
+            res = 1;
+            for (int j = 1; j + 2 * i <= n; ++j) {
+                if (lcp[i][i + 1] >= j) res = max(res, dfs(i + j) + 1);
+            }
+            return res;
+        };
+        return dfs(0);
     }
 };
 int main() {
