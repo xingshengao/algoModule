cf666af0ef69ae714f6732b06ba0e8387b5a8bb4
Khosaka Reina <2211916133@qq.com>
2023-12-17 14:21:56 +0800

    12177

--- template/杂/预处理回文数.cpp
+++ template/杂/预处理回文数.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> f() {
-    vector<int> A;  // 高效枚举40000内的回文数
-    for (int i = 1; i < 400; i++) {
-        int p = i;
-        for (int x = i / 10; x > 0; x /= 10) {
+using LL = long long;
+vector<LL> A;
+// 这种写法, 可以保证只执行一次
+int init = []() {
+    // 高效枚举1e9内的回文数
+    for (LL i = 1; i <= 100000; i++) {
+        // 第一部分 12345->123454321
+        LL p = i;
+        for (LL x = i / 10; x > 0; x /= 10) {
             p = p * 10 + x % 10;
         }
-        A.push_back(p);
-        if (i < 100) {
-            p = i;
-            for (int x = i; x > 0; x /= 10) {
-                p = p * 10 + x % 10;
-            }
-            A.push_back(p);
+        if (p < 1e9) A.push_back(p);
+        // 第二部分 12345->1234554321
+        p = i;
+        for (LL x = i; x > 0; x /= 10) {
+            p = p * 10 + x % 10;
         }
+        if (p < 1e9) A.push_back(p);
     }
-    return A;
-}
+    sort(A.begin(), A.end());
+    return 0;
+}();
