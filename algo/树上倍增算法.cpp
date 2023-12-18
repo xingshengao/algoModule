90f3a856dcd144b5eec6349ad1e65fba6b77777f
xingshengao <2211916133@qq.com>
2023-12-18 23:45:04 +0800

    1218

--- algo/树上倍增算法.cpp
+++ algo/树上倍增算法.cpp
@@ -5,6 +5,10 @@ using namespace std;
  * 在构造函数中, 初始化每个节点x的第2^i个祖先节点, 记作pa[x][i], 若第2^i个祖先节点不存在则pa[x][i]=-1
  * pa[x][i] = pa[pa[x][i]][i], 表示x的第2^i个祖先节点的第2^i祖先节点, 就是x的第2^(i+1)个祖先节点
  */
+// __builtin_popcount(int) 二进制中1的个数, __builtin_popcountll()
+// 32 - __builtin_clz(int)二进制i长度数, 64 - __builtin_clzll(LL)
+// clz本身是返回括号内数的二进制表示数前导0的个数
+// __builtin_ctz(int) 返回括号内数的二进制表示数末尾0的个数, 集合中的最小元素, __builtin_ctzll()
 class TreeAncestor {
    public:
     vector<vector<int>> pa;
@@ -35,7 +39,7 @@ class TreeAncestor {
     }
     // 另一种写法，不断去掉 k 的最低位的 1
     int getKthAncestor2(int node, int k) {
-        for (; k && node != -1; k &= k - 1) // 也可以写成 ~node
+        for (; k && node != -1; k &= k - 1)  // 也可以写成 ~node
             node = pa[node][__builtin_ctz(k)];
         return node;
     }
