6d25224b56682fac294b540fd72c4e3ca75ce317
xingshengao <2211916133@qq.com>
2023-12-28 21:40:13 +0800

    1228

--- template/数学/埃氏筛.cpp
+++ template/数学/埃氏筛.cpp
@@ -1,8 +1,21 @@
 // 质数筛
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 1;
-int main() {
+// 预处理所有质因子
+const int N = 100010;
+vector<int> fac[N + 10];
+// 预处理每个数的质因子
+int init = []() {
+    for (int i = 2; i <= N; ++i) {
+        if (fac[i].size() == 0) {
+            for (int j = i; j <= N; j += i) fac[j].push_back(i);
+        }
+    }
+    return 0;
+}();
+// 质数筛
+void solve() {
+    const int N = 1e5 + 1;
     bool isPrime[N];
     memset(isPrime, true, sizeof(isPrime));
     isPrime[1] = false;
@@ -15,5 +28,7 @@ int main() {
             }
         }
     }
+}
+int main() {
     return 0;
 }
