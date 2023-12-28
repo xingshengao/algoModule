935671873680952f3b38363a769aca533e8b8d47
Khosaka Reina <2211916133@qq.com>
2023-12-28 18:49:36 +0800

    1128

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,10 +3,28 @@
 using namespace std;
 const double PI = acos(-1);
 typedef pair<int, int> PII;
+typedef pair<long long, long long> PLL;
 typedef long long LL;
 vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+vector<pair<int, int>> dirs8 = {{-1, -1}, {-1, 1}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {1, 0}};
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
+long long fpow(long long x, long long exp, long long mod) {  // 快速幂
+    long long res = 1;
+    for (; exp; exp /= 2) {
+        if (exp & 1) res = res * x % mod;
+        x = x * x % mod;
+    }
+    return res;
+}
+long long fpow(long long x, long long exp) {  // 快速幂
+    long long res = 1;
+    for (; exp; exp /= 2) {
+        if (exp & 1) res = res * x;
+        x = x * x;
+    }
+    return res;
+}
 string to_string(string s) { return '"' + s + '"'; }
 
 string to_string(const char* s) { return to_string((string)s); }
@@ -23,9 +41,7 @@ string to_string(A v) {
     bool first = true;
     string res = "{";
     for (const auto& x : v) {
-        if (!first) {
-            res += ", ";
-        }
+        if (!first) res += ", ";
         first = false;
         res += to_string(x);
     }
@@ -54,51 +70,69 @@ void mydebug(const char* format, Head H, Tail... T) {
 }
 #define debug(...) mydebug(#__VA_ARGS__, __VA_ARGS__)
 
-static constexpr long long mod = 998244353;
-bool check(string& a, string& b) {
-    int cnt = 0;
-    for (int i = 0; i < a.size(); ++i) {
-        cnt += a[i] != b[i];
-        if (cnt > 1) return false;
-    }
-    return cnt == 1;
-}
+// static constexpr long long mod = 998244353;
+static constexpr long long mod = 1000000007;
 void solve() {
-    int N, Q;
-    cin >> N >> Q;
-    vector<int> deg(N);
-    vector<set<int>> e(N);
-    int ans = N;
-    while (Q--) {
-        int o;
-        cin >> o;
-        if (o == 1) {
-            int u, v;
-            cin >> u >> v;
-            u--, v--;
-            ans -= !deg[u]++;
-            ans -= !deg[v]++;
-            e[u].insert(v);
-            e[v].insert(u);
-        } else {
-            int u;
-            cin >> u;
-            u--;
-            ans += (deg[u] > 0);
-            deg[u] = 0;
-            for (auto v : e[u]) {
-                ans += !--deg[v];
-                e[v].erase(u);
-            }
-            e[u].clear();
-        }
-        cout << ans << "\n";
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<vector<int>> g(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
+    // 是否存在连续三个节点颜色相同, 相同输出NO
+    bool ans = true;
+    function<void(int, int)> dfs = [&](int x, int fa) {
+        if (ans = false) return;
+        if (fa == -1) {
+            if (g[x].size() == 2 and s[x] == s[g[x][0]] == s[g[x][1]]) {
+                ans = false;
+                cout << x << endl;
+            }
+        } else {
+            if (g[x].size() >= 2) {
+                // 除父节点外, 至少有一个子节点
+                vector<int> color;
+                color.push_back(s[fa]);
+                color.push_back(s[x]);
+                for (int y : g[x]) {
+                    if (y == fa) continue;
+                    color.push_back(s[y]);
+                }
+                int a = 0, b = 0;
+                for (int c : color) {
+                    if (c == 0) a += 1;
+                    if (c == 1) a += 2;
+                }
+                if (a >= 3 or b >= 3) {
+                    ans = false;
+                    cout << x << endl;
+                    return;
+                }
+            }
+        }
+        for (int y : g[x]) {
+            if (y != fa) {
+                dfs(y, x);
+            }
+        }
+    };
+    dfs(0, -1);
+    if (ans)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
+
 signed main() {
     std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while (T--) {
         solve();
     }
--- template/数学/埃氏筛.cpp
+++ template/数学/埃氏筛.cpp
@@ -1,8 +1,20 @@
 // 质数筛
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 1;
-int main() {
+// 预处理每个数字的质因子
+const int N = 100010;
+vector<int> fac[N + 10];
+int init = []() {
+    for (int i = 2; i <= N; ++i) {
+        if (fac[i].size() == 0) {
+            for (int j = i; j <= N; j += i) fac[j].push_back(i);
+        }
+    }
+    return 0;
+}();
+// 埃氏筛质数
+void solve() {
+    const int N = 1e5 + 1;
     bool isPrime[N];
     memset(isPrime, true, sizeof(isPrime));
     isPrime[1] = false;
@@ -15,5 +27,5 @@ int main() {
             }
         }
     }
-    return 0;
 }
+int main() { return 0; }
