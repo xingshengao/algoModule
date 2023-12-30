1bc2eb7ffe30edd64ded2c83f4c9f5b0522bc1ad
xingshengao <2211916133@qq.com>
2023-12-30 22:49:04 +0800

    Update 取模类.cpp

--- template/数学/取模类.cpp
+++ template/数学/取模类.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 using LL = long long;
-using i64 = long long;
 template <class T>
-constexpr T power(T a, i64 b) {
+constexpr T power(T a, LL b) {
     T res = 1;
     for (; b; b /= 2, a *= a) {
         if (b % 2) {
@@ -14,30 +13,30 @@ constexpr T power(T a, i64 b) {
     return res;
 }
 
-constexpr i64 mul(i64 a, i64 b, i64 p) {
-    i64 res = a * b - i64(1.L * a * b / p) * p;
+constexpr LL mul(LL a, LL b, LL p) {
+    LL res = a * b - LL(1.L * a * b / p) * p;
     res %= p;
     if (res < 0) {
         res += p;
     }
     return res;
 }
-template <i64 P>
+template <LL P>
 struct MLong {
-    i64 x;
+    LL x;
     constexpr MLong() : x{} {}
-    constexpr MLong(i64 x) : x{norm(x % getMod())} {}
+    constexpr MLong(LL x) : x{norm(x % getMod())} {}
 
-    static i64 Mod;
-    constexpr static i64 getMod() {
+    static LL Mod;
+    constexpr static LL getMod() {
         if (P > 0) {
             return P;
         } else {
             return Mod;
         }
     }
-    constexpr static void setMod(i64 Mod_) { Mod = Mod_; }
-    constexpr i64 norm(i64 x) const {
+    constexpr static void setMod(LL Mod_) { Mod = Mod_; }
+    constexpr LL norm(LL x) const {
         if (x < 0) {
             x += getMod();
         }
@@ -46,8 +45,8 @@ struct MLong {
         }
         return x;
     }
-    constexpr i64 val() const { return x; }
-    explicit constexpr operator i64() const { return x; }
+    constexpr LL val() const { return x; }
+    explicit constexpr operator LL() const { return x; }
     constexpr MLong operator-() const {
         MLong res;
         res.x = norm(getMod() - x);
@@ -91,7 +90,7 @@ struct MLong {
         return res;
     }
     friend constexpr std::istream& operator>>(std::istream& is, MLong& a) {
-        i64 v;
+        LL v;
         is >> v;
         a = MLong(v);
         return is;
@@ -102,13 +101,13 @@ struct MLong {
 };
 
 template <>
-i64 MLong<0LL>::Mod = i64(1E18) + 9;
+LL MLong<0LL>::Mod = LL(1E18) + 9;
 
 template <int P>
 struct MInt {
     int x;
     constexpr MInt() : x{} {}
-    constexpr MInt(i64 x) : x{norm(x % getMod())} {}
+    constexpr MInt(LL x) : x{norm(x % getMod())} {}
 
     static int Mod;
     constexpr static int getMod() {
@@ -173,7 +172,7 @@ struct MInt {
         return res;
     }
     friend constexpr std::istream& operator>>(std::istream& is, MInt& a) {
-        i64 v;
+        LL v;
         is >> v;
         a = MInt(v);
         return is;
