9fe97a6d6c6d16ca6256fe3d85ed0b62b6411fe1
Khosaka Reina <2211916133@qq.com>
2024-01-02 13:32:09 +0800

    Update 埃氏筛.cpp

--- template/数学/埃氏筛.cpp
+++ template/数学/埃氏筛.cpp
@@ -29,11 +29,11 @@ void solve() {
         }
     }
 }
-// 质数筛
+// // 质数筛
 // const int N = 1000100;
 // int isPrime[N];
 // int init = []() {
-//     memset(isPrime, 1, sizeof(isPrime));
+//     for (int i = 0; i < N; ++i) isPrime[i] = 1;
 //     isPrime[1] = 1;
 //     isPrime[2] = 1;
 //     for (int i = 2; i * i < N; ++i) {
