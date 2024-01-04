7ca8ba5b58505bee87a46ed1e02729f2f978c89b
xingshengao <2211916133@qq.com>
2024-01-04 21:30:30 +0800

    Create 离散化粘贴.cpp

---
+++ template/线段树/离散化粘贴.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using LL = long long;
+
+void f() {
+    // 离散化
+    auto Uni = [](vector<LL>& A) -> vector<LL> {
+        vector<LL> ans(A);
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        return ans;
+    };
+    vector<LL> A = Uni(arr);
+    map<LL, LL> mp;
+    for (int i = 0; i < A.size(); ++i) {
+        mp[A[i]] = i + 1;
+    }
+}
