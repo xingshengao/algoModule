0274ce1b36c4c59a9b7894de1ab25c8cfd9ef926
Khosaka Reina <2211916133@qq.com>
2024-01-05 13:38:16 +0800

    Update Fenwick.cpp

--- template/树状数组/Fenwick.cpp
+++ template/树状数组/Fenwick.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+// 开区间线段树
 template <typename T>
 struct Fenwick {
     int n;
@@ -37,3 +41,33 @@ struct Fenwick {
         return x;
     }
 };
+
+// 重载MAX树状数组
+struct Info {
+    LL x;
+    Info() { x = -1e18; }
+    Info(LL val) { x = val; }
+    Info operator+=(Info& rhs) {
+        this->x = max(this->x, rhs.x);
+        return *this;
+    }
+};
+
+void LiSanHua() {  // 离散化的代码
+    vector<LL> arr;
+    // 要进行树状数组前缀和的点去重
+    auto Uni = [](vector<LL>& A) -> vector<LL> {
+        vector<LL> ans(A);
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        return ans;
+    };
+    // 要进行离散化的数组, 可以是x可以是y
+    vector<LL> A = Uni(arr);
+    // 离散化
+    map<LL, LL> mp;
+    for (int i = 0; i < A.size(); ++i) {
+        mp[A[i]] = i + 1;
+    }
+    Fenwick<LL> fen(A.size() + 1);
+}
