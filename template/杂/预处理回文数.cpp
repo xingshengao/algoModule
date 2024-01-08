895b7ed869d30826e841cde4ff7e7bed0ccb8e1b
Khosaka Reina <2211916133@qq.com>
2024-01-08 19:27:28 +0800

    Update 预处理回文数.cpp

--- template/杂/预处理回文数.cpp
+++ template/杂/预处理回文数.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 using LL = long long;
 vector<LL> A;
-// 这种写法, 可以保证只执行一次
-int init = []() {
-    // 高效枚举1e9内的回文数
+int init = []() {  // 预处理1e9内的所有回文数字, 这种写法, 可以保证只执行一次
     for (LL i = 1; i <= 100000; i++) {
         // 第一部分 12345->123454321
         LL p = i;
@@ -26,10 +25,8 @@ int init = []() {
 // 1e11内的所有回文数字
 namespace EE11 {
 using LL = long long;
-// 预处理1e11内的所有回文数字
 vector<LL> A;
-// 这种写法, 可以保证只执行一次
-int init = []() {
+int init = []() {  // 预处理1e13内的所有回文数字, 这种写法, 可以保证只执行一次
     // 高效枚举1e11内的回文数
     for (LL i = 1; i <= 1000000; i++) {
         // 第一部分 12345->123454321
@@ -49,3 +46,27 @@ int init = []() {
     return 0;
 }();
 };  // namespace EE11
+
+// 1e13内的所有回文数
+namespace EE13 {
+using LL = long long;
+vector<LL> A;
+int init = []() {  // 预处理1e13内的所有回文数字, 这种写法, 可以保证只执行一次
+    for (LL i = 1; i <= 10000000; i++) {
+        // 第一部分 12345->123454321
+        LL p = i;
+        for (LL x = i / 10; x > 0; x /= 10) {
+            p = p * 10 + x % 10;
+        }
+        if (p < 1e13) A.push_back(p);
+        // 第二部分 12345->1234554321
+        p = i;
+        for (LL x = i; x > 0; x /= 10) {
+            p = p * 10 + x % 10;
+        }
+        if (p < 1e13) A.push_back(p);
+    }
+    sort(A.begin(), A.end());
+    return 0;
+}();
+};  // namespace EE13
