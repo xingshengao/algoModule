c1dfc7c52627d29ffeb251bfa195cbf49a131101
xingshengao <2211916133@qq.com>
2024-01-23 01:34:57 +0800

    Update cppMain.cpp

--- cppMain.cpp
+++ cppMain.cpp
@@ -84,6 +84,47 @@ struct TreeNode {
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
 
+template <typename T>
+ostream& operator<<(ostream& out, const set<T>& obj) {
+    out << "set(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+template <typename T>
+ostream& operator<<(ostream& out, const unordered_set<T>& obj) {
+    out << "unordered_set(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const map<T1, T2>& obj) {
+    out << "map(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << it->first << ": " << it->second;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const unordered_map<T1, T2>& obj) {
+    out << "unordered_map(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << it->first << ": " << it->second;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const pair<T1, T2>& obj) {
+    out << "<" << obj.first << ", " << obj.second << ">";
+    return out;
+}
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& obj) {
+    out << "vector(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+
 string to_string(string s) {
     return '"' + s + '"';
 }
