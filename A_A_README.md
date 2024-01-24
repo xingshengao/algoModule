f4a8cd3192732d39ebf413ac1ec94760479af6fb
Khosaka Reina <2211916133@qq.com>
2024-01-24 15:48:07 +0800

    更改

---
+++ A_A_README.md
@@ -0,0 +1,13 @@
+## 个人代码模板仓库
+**Xsa_DeleteMemo**
+
+
+### 二进制笔记
+`__builtin_popcount(int x)` 统计1的数量 `__builtin_popcountll(LL x)`
+
+`__builtin_ctz(int x)` 后缀0的个数, `__builtin_ctzll(LL x)`
+
+`__builtin_clz(int x)` 从最高位开始连续0的个数, 可以用来计算二进制长度(`32 - __builtin_clz(int x)`, `64 - __builtin_clzll(LL x)`)
+
+
+`二进制加法器a + b = (a ^ b) + 2 * (a & b)`
---
+++ D_Devu_and_his_Brother.cpp
@@ -0,0 +1,240 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+#define all(c) c.begin(), c.end()
+#define REP(i, a, b) for (int i = a; i < (b); i++)
+#define RREP(i, a, b) for (int i = a; i >= b; i--)
+using LL = long long;
+using VI = vector<int>;
+using VL = vector<LL>;
+using VVI = vector<vector<int>>;
+using VVL = vector<vector<LL>>;
+using VD = vector<double>;
+using VVD = vector<vector<double>>;
+using PII = pair<int, int>;
+using PLL = pair<long long, long long>;
+using TIII = tuple<int, int, int>;
+using TLLL = tuple<LL, LL, LL>;
+using VPII = vector<PII>;
+using VVVI = vector<vector<vector<int>>>;
+using VVVL = vector<vector<vector<LL>>>;
+
+template <class T>
+void mkuni(vector<T>& v) {
+    sort(all(v));
+    v.erase(unique(all(v)), v.end());
+}
+
+template <class T, class S = T>
+S SUM(const vector<T>& a) {
+    return accumulate(a.begin(), a.end(), S(0));
+}
+
+template <class T>
+T MAX(const vector<T>& a) {
+    return *max_element(a.begin(), a.end());
+}
+
+template <class T>
+T MIN(const vector<T>& a) {
+    return *min_element(a.begin(), a.end());
+}
+
+template <class T>
+bool chmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0;
+}  // set a = min(a,b)
+
+template <class T>
+bool chmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0;
+}  // set a = max(a,b)
+
+const double PI = acos(-1);
+vector<pair<int, int>> dirs8 = {{-1, -1}, {-1, 1}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {1, 0}};
+vector<pair<int, int>> dirs4 = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+
+long long fpow(long long x, long long exp, long long mod) {  // 快速幂
+    long long res = 1;
+    for (; exp; exp /= 2) {
+        if (exp & 1) res = res * x % mod;
+        x = x * x % mod;
+    }
+    return res;
+}
+
+long long fpow(long long x, long long exp) {  // 快速幂
+    long long res = 1;
+    for (; exp; exp /= 2) {
+        if (exp & 1) res = res * x;
+        x = x * x;
+    }
+    return res;
+}
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+template <typename T>
+ostream& operator<<(ostream& out, const set<T>& obj) {
+    out << "set(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+template <typename T>
+ostream& operator<<(ostream& out, const unordered_set<T>& obj) {
+    out << "unordered_set(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const map<T1, T2>& obj) {
+    out << "map(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << it->first << ": " << it->second;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const unordered_map<T1, T2>& obj) {
+    out << "unordered_map(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << it->first << ": " << it->second;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const pair<T1, T2>& obj) {
+    out << "<" << obj.first << ", " << obj.second << ">";
+    return out;
+}
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& obj) {
+    out << "vector(";
+    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+
+string to_string(string s) { return '"' + s + '"'; }
+
+string to_string(const char* s) { return to_string((string)s); }
+
+string to_string(bool b) { return (b ? "true" : "false"); }
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) res += ", ";
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+void debug_out() { cout << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cout << " " << to_string(H);
+    debug_out(T...);
+}
+
+template <typename T>
+void mydebug(const char* format, T t) {
+    cout << format << "=" << to_string(t) << endl;
+}
+
+template <typename Head, typename... Tail>
+void mydebug(const char* format, Head H, Tail... T) {
+    while (*format != ',') cout << *format++;
+    cout << "=" << to_string(H) << ",";
+    mydebug(format + 1, T...);
+}
+#define debug(...) mydebug(#__VA_ARGS__, __VA_ARGS__)
+
+static constexpr long long mod = 998244353;
+// static constexpr long long mod = 1000000007;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < m; ++i) cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    // 计算代价, 使得min(a) >= x and max(b) <= x
+    function<int(int)> calc = [&](int x) -> int {
+        int res = 0;
+        for (int v : a) {
+            if (v < x) {
+                res += x - v;
+            }
+        }
+        for (int v : b) {
+            if (v > x) {
+                res += v - x;
+            }
+        }
+        return res;
+    };
+    int l = 0, r = 1000000002;
+    // // 写法1, 抄的cf大佬的
+    // while (l < r) {
+    //     int m1 = (l + l + r) / 3, m2 = (l + r + r) / 3;
+    //     if (calc(m1) > calc(m2)) { // 舍去左边这一段
+    //         l = m1 + (l == m1);
+    //     } else { // 舍去右边这一段
+    //         r = m2 - (r == m2);
+    //     }
+    // }
+
+    // 写法2
+    while (l + 3 <= r) {
+        int m1 = l + (r - l) / 3;
+        int m2 = r - (r - l) / 3;
+        if (calc(m1) < calc(m2)) {  // lmid < rmid, 舍去右边这一段
+            r = m2;
+        } else {  // lmid > rmid, 舍去左边这一段
+            l = m1;
+        }
+    }
+    // 最终函数转折点在区间[l, r]之间
+    int ans = 1e18;
+    for (int x = l; x <= r; ++x) {
+        ans = min(ans, calc(x));
+    }
+    cout << ans << endl;
+}
+
+signed main() {
+    std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
