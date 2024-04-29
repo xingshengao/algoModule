9b5c3fa5cd6b5d5770d3f3f093147e60a36fc1da
Khosaka Reina <2211916133@qq.com>
2024-04-29 16:18:21 +0800

    Update 3.cpp

--- 笔试/2024.04.27美团笔试/3.cpp
+++ 笔试/2024.04.27美团笔试/3.cpp
@@ -1,188 +1,35 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-
-#define all(c) c.begin(), c.end()
-#define REP(i, a, b) for (int i = a; i < (b); i++)
-#define RREP(i, a, b) for (int i = a; i >= b; i--)
-#define print(x) cout << x << endl
-using LL = long long;
-using VI = vector<int>;
-using VL = vector<LL>;
-using VVI = vector<vector<int>>;
-using VVL = vector<vector<LL>>;
-using VD = vector<double>;
-using VVD = vector<vector<double>>;
-using PII = pair<int, int>;
-using PLL = pair<long long, long long>;
-using TIII = tuple<int, int, int>;
-using TLLL = tuple<LL, LL, LL>;
-using VPII = vector<PII>;
-using VVVI = vector<vector<vector<int>>>;
-using VVVL = vector<vector<vector<LL>>>;
-
-template <class T>
-void mkuni(vector<T>& v) {
-    sort(all(v));
-    v.erase(unique(all(v)), v.end());
-}
-
-template <class T, class S = T>
-S SUM(const vector<T>& a) {
-    return accumulate(a.begin(), a.end(), S(0));
-}
-
-template <class T>
-T MAX(const vector<T>& a) {
-    return *max_element(a.begin(), a.end());
-}
-
-template <class T>
-T MIN(const vector<T>& a) {
-    return *min_element(a.begin(), a.end());
-}
-
-template <class T>
-bool chmin(T& a, const T& b) {
-    return b < a ? a = b, 1 : 0;
-}  // set a = min(a,b)
-
-template <class T>
-bool chmax(T& a, const T& b) {
-    return a < b ? a = b, 1 : 0;
-}  // set a = max(a,b)
-
-const double PI = acos(-1);
-vector<pair<int, int>> dirs8 = {{-1, -1}, {-1, 1}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {1, 0}};
-vector<pair<int, int>> dirs4 = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
-
-long long fpow(long long x, long long exp, long long mod) {  // 快速幂
-    long long res = 1;
-    for (; exp; exp /= 2) {
-        if (exp & 1) res = res * x % mod;
-        x = x * x % mod;
-    }
-    return res;
-}
-
-long long fpow(long long x, long long exp) {  // 快速幂
-    long long res = 1;
-    for (; exp; exp /= 2) {
-        if (exp & 1) res = res * x;
-        x = x * x;
-    }
-    return res;
-}
-bool isdig(char x) { return x >= '0' && x <= '9'; }
-bool isup(char x) { return x >= 'A' && x <= 'Z'; }
-bool isdown(char x) { return x >= 'a' && x <= 'z'; }
-bool islet(char x) { return isup(x) || isdown(x); }
-
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-};
-
-template <typename T>
-ostream& operator<<(ostream& out, const set<T>& obj) {
-    out << "set(";
-    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
-    out << ")";
-    return out;
-}
-template <typename T>
-ostream& operator<<(ostream& out, const unordered_set<T>& obj) {
-    out << "unordered_set(";
-    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
-    out << ")";
-    return out;
-}
-template <typename T1, typename T2>
-ostream& operator<<(ostream& out, const map<T1, T2>& obj) {
-    out << "map(";
-    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << it->first << ": " << it->second;
-    out << ")";
-    return out;
-}
-template <typename T1, typename T2>
-ostream& operator<<(ostream& out, const unordered_map<T1, T2>& obj) {
-    out << "unordered_map(";
-    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << it->first << ": " << it->second;
-    out << ")";
-    return out;
-}
-template <typename T1, typename T2>
-ostream& operator<<(ostream& out, const pair<T1, T2>& obj) {
-    out << "<" << obj.first << ", " << obj.second << ">";
-    return out;
-}
-template <typename T>
-ostream& operator<<(ostream& out, const vector<T>& obj) {
-    out << "vector(";
-    for (auto it = obj.begin(); it != obj.end(); it++) out << (it == obj.begin() ? "" : ", ") << *it;
-    out << ")";
-    return out;
-}
-
-string to_string(string s) { return '"' + s + '"'; }
-
-string to_string(const char* s) { return to_string((string)s); }
-
-string to_string(bool b) { return (b ? "true" : "false"); }
-
-template <typename A, typename B>
-string to_string(pair<A, B> p) {
-    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
-}
-
-template <typename A>
-string to_string(A v) {
-    bool first = true;
-    string res = "{";
-    for (const auto& x : v) {
-        if (!first) res += ", ";
-        first = false;
-        res += to_string(x);
-    }
-    res += "}";
-    return res;
-}
-
-void debug_out() { cout << endl; }
-
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
-    cout << " " << to_string(H);
-    debug_out(T...);
-}
-
-template <typename T>
-void mydebug(const char* format, T t) {
-    cout << format << "=" << to_string(t) << endl;
-}
-
-template <typename Head, typename... Tail>
-void mydebug(const char* format, Head H, Tail... T) {
-    while (*format != ',') cout << *format++;
-    cout << "=" << to_string(H) << ",";
-    mydebug(format + 1, T...);
-}
-#define debug(...) mydebug(#__VA_ARGS__, __VA_ARGS__)
-
-static constexpr long long mod = 998244353;
 // static constexpr long long mod = 1000000007;
+// 数组合并，给定一个分割数组最小值，求有多少种分割的方法
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    // dfs(i, j)代表考虑从i往后的数字，当前和为j，组成的满足的方案数量是多少
+    int sum = accumulate(a.begin(), a.end(), 0LL);
+    vector memo(n + 10, vector<int>(sum + 1, -1));
+    function<int(int, int)> dfs = [&](int i, int j) -> int {
+        if (i == n) {
+            if (j >= k)
+                return 1;
+            else
+                return 0;
+        }
+        if (memo[i][j] != -1) return memo[i][j];
+        int res = 0;
+        if (j >= k)
+            res += dfs(i + 1, a[i]) + dfs(i + 1, j + a[i]);  // 新开一个分组或者继续这个分组
 
-void solve() {}
+        else
+            res += dfs(i + 1, j + a[i]);  // 和不够k只能继续这个分组
+        res = (res % mod + mod) % mod;
+        return memo[i][j] = res;
+    };
+    cout << dfs(0, 0) << endl;
+}
 
 signed main() {
     std::ios::sync_with_stdio(0), std::cout.tie(0), std::cin.tie(0);
